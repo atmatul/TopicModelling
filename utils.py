d9ef105b93f7f001832d77661f8cc149ff79691d
Mohan,Atul <am418877@ima-ifu.rwth-aachen.de>
2019-08-01 13:16:02 +0200

    Word CLound, Bigram and Trigram

--- utils.py
+++ utils.py
@@ -13,12 +13,27 @@ from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
 
-import pyLDAvis
-import pyLDAvis.gensim
+from gensim.models import TfidfModel
+from gensim.corpora import Dictionary
+
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+
+from matplotlib import pyplot as plt
+
+from wordcloud import WordCloud
 
 stop_words = set(stopwords.words('english'))
 stemmer = PorterStemmer()
 lemmatizer = WordNetLemmatizer()
+wc = WordCloud(
+    background_color="white",
+    max_words=20,
+    width=1024,
+    height=720,
+    stopwords=stopwords.words("english"),
+    colormap='tab10'
+)
 
 
 def readfiles(path: str):
@@ -116,22 +131,129 @@ def clean_abstract(entry: dict):
     except Exception as e:
         print(e)
 
+def bigrams(pds_corpus, num):
+    try:
+        # vectorized content
+        vector = CountVectorizer(ngram_range=(2, 2), stop_words='english').fit(pds_corpus)
+        bow = vector.transform(pds_corpus)
+        sum_words = bow.sum(axis=0)
 
-def visualize_lda_model(model, corpus, corpus_dict):
+        # word freq
+        words_freq = [(word, sum_words[0, idx]) for word, idx in vector.vocabulary_.items()]
+
+        # descending sort
+        words_freq = sorted(words_freq, key=lambda entry: entry[1], reverse=True)
+
+        df_bigram = pd.DataFrame(words_freq, columns=['word', 'count'])
+
+        df_bigram['count'] = (df_bigram['count'] - df_bigram['count'].min()) / (
+                df_bigram['count'].max() - df_bigram['count'].min())
+
+        df_bigram.sort_values(by=['count'])
+
+        # write_to_excel(data=df_bigram.head(num), title='df_bigram_norm')
+
+        # plot top num : pd.Dataframe, columns = ['words', 'count']
+        df_bigram.head(num).groupby('word').sum()['count'].plot(kind='bar',
+                                                                title='Top {0} Bigram Distribution'.format(num))
+        plt.xlabel('Bigrams')
+        plt.ylabel('Percent')
+        # plt.savefig('top_{0}_bigram.png'.format(num), format='png', dpi=100)
+        plt.show()
+
+        return df_bigram
+
+    except Exception as e:
+        print(e)
+
+
+def trigram_analysis(pds_corpus, num):
     """
-    Method to visualize LDA results
-    :param model: lda model
-    :param corpus: bow
-    :param corpus_dict: id2word
-    :return: Bool
+    Method to vectorize the content of the corpus and return top #num words
+    :param pds_corpus: pd.Series, corpus content
+    :param num: int, number of top n results to return
+    :return: None
     """
-    vis = pyLDAvis.gensim.prepare(model, corpus, corpus_dict)
-    pyLDAvis.save_html(vis, 'LDA_Visualization.html')
-    return True
+    try:
+        # vectorized content
+        vector = CountVectorizer(ngram_range=(3, 3), stop_words='english').fit(pds_corpus)
+        bow = vector.transform(pds_corpus)
+        sum_words = bow.sum(axis=0)
+
+        # word freq
+        words_freq = [(word, sum_words[0, idx]) for word, idx in vector.vocabulary_.items()]
+
+        # descending sort
+        words_freq = sorted(words_freq, key=lambda entry: entry[1], reverse=True)
+
+        df_trigram = pd.DataFrame(words_freq, columns=['word', 'count'])
+
+        df_trigram['count'] = (df_trigram['count'] - df_trigram['count'].min()) / (
+                df_trigram['count'].max() - df_trigram['count'].min())
+
+        df_trigram.sort_values(by=['count'])
+
+        # write_to_excel(data=df_trigram.head(num), title='df_trigram_norm')
+
+        # plot top num : pd.Dataframe, columns = ['words', 'count']
+        df_trigram.head(num).groupby('word').sum()['count'].plot(kind='bar',
+                                                                 title='Top {0} Trigram Distribution'.format(num))
+        plt.xlabel('Trigrams')
+        plt.ylabel('Percent')
+        # plt.savefig('top_{0}_trigram.png'.format(num), format='png', dpi=100)
+        plt.show()
+
+    except Exception as e:
+        print(e)
+
+
+def word_cloud_analysis(df_corpus, lst_clean):
+    """
+    Method to provide tf-idf analysis on the corpus
+    :param df_corpus: pd.Dataframe, corpus
+    :return:
+    """
+    try:
+        clean_corpus = lst_clean
+        freq = {}
+
+        # convert to corpus dict
+        corpus_dict = Dictionary(clean_corpus)
+
+        # convert to vector corpus
+        vectors = [corpus_dict.doc2bow(text) for text in clean_corpus]
+
+        # build tf-idf
+        tfidf = TfidfModel(vectors)
+        weights = tfidf[vectors[0]]
+
+        # pair words with weights
+        for pair in weights:
+            if corpus_dict[pair[0]] not in freq.keys():
+                freq[corpus_dict[pair[0]]] = pair[1]
+            else:
+                freq[corpus_dict[pair[0]]] = float(sum(freq.get(corpus_dict[pair[0]]), pair[1]) / 2)
+
+        # generate word cloud
+        wc.generate_from_frequencies(freq)
+
+        # save to file
+        wc.to_file("word_cloud.png")
+
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
     bibfiles = readfiles(path=os.path.join(os.getcwd(), 'data'))
     lst_files = parsebibfiles(bibfiles)
     lst_clean_bow = [clean_abstract(file) for file in lst_files]
-    print(lst_clean_bow)
+
+    pds_corpus = pd.Series([item.get('abstract') for item in lst_files])
+
+    # bigrams(pds_corpus, 10)
+    # trigram_analysis(pds_corpus, 10)
+
+    word_cloud_analysis(pds_corpus, lst_clean_bow)
+
+    print("Done")
